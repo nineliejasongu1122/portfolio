2af13fc7313dda55dafd1c6fa16d174ff4e2c351
Yaochen Gu <yaochen.gu.2020@scis.smu.edu.sg>
2023-02-05 17:52:54 +0800

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Contact = () => {
   const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
   const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
+  const [buttonText, setButtonText] = useState("Send");
   const [status, setStatus] = useState({});
 
   const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -38,9 +38,12 @@ export const Contact = () => {
     let result = await response.json();
     setFormDetails(formInitialDetails);
     if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
+      setStatus({ succes: true, message: "Message sent successfully" });
     } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+      setStatus({
+        succes: false,
+        message: "Something went wrong, please try again later.",
+      });
     }
   };
 
@@ -92,5 +95,5 @@ export const Contact = () => {
         </Row>
       </Container> */}
     </section>
-  )
-}
+  );
+};
